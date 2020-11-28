54872d8adb8be0717ed697e2c726373c08700806
Anatoly Poluyaktov <avpoluyaktov@gmail.com>
2020-11-28 23:26:21 +0300

    added laba 3

---
+++ lab3/client.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SOCK_PATH "echo_socket"
+int main()
+{     
+	     char buffer[256+1];
+    FILE* fp = popen("who -u | cut -d \" \" -f1", "r");
+    size_t read_chars;
+    size_t result_size = 0;
+    char* users = (char*) calloc(256+1, sizeof(char));
+    if ( users == NULL)
+    {
+        perror("problems with allocating");
+        return 1;
+    }
+
+    while( (read_chars = fread(buffer, 1, sizeof(buffer), fp) ) )
+    {
+
+        users = realloc(users, (result_size + read_chars)+1);
+
+        memmove(users + result_size, buffer, read_chars);
+        result_size += read_chars;
+    }
+
+    pclose(fp);
+    char str[strlen(users)];
+
+    strcpy(str,users);
+    free(users);
+    str[strlen(str)-1]='\0';
+    printf("%s\n",str);
+
+	 int s, len;
+    struct sockaddr_un remote;
+
+    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    printf("Trying to connect...\n");
+
+    remote.sun_family = AF_UNIX;
+    strcpy(remote.sun_path, SOCK_PATH);
+    len = strlen(remote.sun_path) + sizeof(remote.sun_family);
+    if (connect(s, (struct sockaddr *)&remote, len) == -1) {
+        perror("connect");
+        exit(1);
+    }
+
+    printf("Connected.\n"); 
+    send(s, str, strlen(str), 0);
+    close(s);
+
+	return 0;
+}
---
+++ lab3/server.c
@@ -0,0 +1,66 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define SOCK_PATH "echo_socket"
+int count_users(char* users)
+{	int count = 0;
+	for(int i = 0; i < strlen(users); ++i)
+	{
+		if (users[i] == '\n')
+			++count;
+	}
+		
+	++count;
+	return count;
+	
+}
+int main()
+{    
+	 int s, s2,len;
+	 socklen_t t = 0;
+    struct sockaddr_un local, remote;
+    char str[100];
+
+    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    local.sun_family = AF_UNIX;
+    strcpy(local.sun_path, SOCK_PATH);
+    unlink(local.sun_path);
+    len = strlen(local.sun_path) + sizeof(local.sun_family);
+    if (bind(s, (struct sockaddr *)&local, len) == -1) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(s, 1) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    t = sizeof(remote);
+    if ((s2 = accept(s, (struct sockaddr *)&remote, &t)) == -1) {
+    	perror("accept");
+        exit(1);
+     }
+     memset(str, 0, 100);
+      if (recv(s2, str, sizeof(str),  0) < 0)
+    {
+        printf("recv failed\n");
+        exit(1);
+    }
+    printf("logged users:\n");
+    printf("%s\n", str);
+    
+    int num = count_users(str);
+    printf("number of logged users: %d\n", num);
+ 
+
+    close(s2);
+    
+	return 0;
+}
